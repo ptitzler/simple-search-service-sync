d02b3ec10a3c82183fb25e46680272df4db113fb
Patrick Titzler <ptitzler@us.ibm.com>
2017-02-06 17:11:13 -0800

    i1: throttle change feed

--- app.js
+++ app.js
@@ -16,6 +16,7 @@
 
 'use strict';
 
+const async = require('async');
 const cfenv = require('cfenv');
 const debug = require('debug')('sss-sync');
 const express = require('express');
@@ -25,59 +26,93 @@ const init = require('./lib/initialize.js');
  *
  */
 
- 	debug('debug is enabled.');
+debug('debug is enabled.');
 
-	var appEnv = cfenv.getAppEnv();
+var appEnv = cfenv.getAppEnv();
 
-	// load service binding for repository database if running locally
-	if(appEnv.isLocal) {
-		try {
-	  		appEnv = cfenv.getAppEnv({vcap: {services: require('./vcap_services.json')}});
-		}
-		catch(ex) { 
-			// ignore 
-		}
+// load service binding for repository database if running locally
+if(appEnv.isLocal) {
+	try {
+  		appEnv = cfenv.getAppEnv({vcap: {services: require('./vcap_services.json')}});
+	}
+	catch(ex) { 
+		// ignore 
+	}
+}
+
+debug(JSON.stringify(appEnv));
+
+console.log('Service is initializing...');
+
+// initialize application
+init(appEnv, function(err, feed, sssclient) {
+
+	if(err) {
+		console.error('Initialization failed: ' + err);
+		process.exit(1);
 	}
 
-	debug(JSON.stringify(appEnv));
+	console.log('Service has started.');
 
-	console.log('Service is initializing...');
+	const max_queue_length = 25;
 
-	// initialize application
-	init(appEnv, function(err, feed, sssclient) {
+	// process changes in the source database one-at-a-time in the order they were received
+	const q = async.queue(function(change, asyncCallback) {
+		// Use Simple-Search-Service client to apply change to index
+		sssclient.throttledSync(change,	function(err) {
+									return asyncCallback(err);
+								});
+	},1);
 
-		if(err) {
-			console.error('Initialization failed: ' + err);
-			process.exit(1);
+	var feedpaused = false;
+
+	// invoked when the sync queue is empty
+	q.empty = function() {
+		if(feedpaused) {
+			// resume change feed processing to re-populate the queue
+			debug('Simple-Search-Service sync queue is empty. Resuming change feed.');
+			feed.resume();
+			feedpaused = false;
 		}
+	};
 
-		feed.on('change', function(change) {
-			sssclient.throttledSync(change, function(err) {
-				if(err) {
-					console.log('Simple-search-Service sync failed: ' + err);
-				}
-				else {
-					console.log('Simple-search-Service sync succeeded.');
-				}
-			});
+	// invoked when a document change is reported
+	feed.on('change', function(change) {		
+		// push change to queue
+		q.push(change, function(err) {
+			if(err) {
+				console.error('Simple-search-Service sync failed: ' + err);
+			}
+			else {
+				debug('Simple-search-Service sync for change succeeded.');
+			}
 		});
 
-		feed.on('error', function(err) {
-  			console.error('Error monitoring change feed: ' + err);
-  			throw ('Error monitoring change feed: ' + err);
-		});
-
-		feed.follow();
-
-		var app = express();
-
-		//
-		// start server on the specified port and binding host
-		//
-		app.listen(appEnv.port, '0.0.0.0', function() {
-			console.log('Server starting on ' + appEnv.url);
-		});
+		// if more than <max_queue_length> document changes are held for processing pause the input feed
+		if(q.length() > max_queue_length) {
+			debug('Simple-Search-Service sync queue has reached limit (' + max_queue_length + '). Pausing change feed.');
+			feed.pause();
+			feedpaused = true;
+		}
 	});
 
-	// send sample application deployment tracking request to https://github.com/IBM-Bluemix/cf-deployment-tracker-service
-	//require('cf-deployment-tracker-client').track();
+	// invoked when a fatal error occurred during change feed monitoring
+	feed.on('error', function(err) {
+			console.error('Error monitoring change feed: ' + err);
+			throw ('Error monitoring change feed: ' + err);
+	});
+
+	feed.follow();
+
+	var app = express();
+
+	//
+	// start server on the specified port and binding host
+	//
+	app.listen(appEnv.port, '0.0.0.0', function() {
+		console.log('Server started.');
+	});
+});
+
+// send sample application deployment tracking request to https://github.com/IBM-Bluemix/cf-deployment-tracker-service
+//require('cf-deployment-tracker-client').track();
